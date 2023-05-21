92a426e5288288777f8eef865488b4f1fc539223
326eunjin <326eunjin@naver.com>
2023-05-21 17:36:14 +0900

    division error 해결

--- network_performance.py
+++ network_performance.py
@@ -10,6 +10,7 @@ def measure_network_performance(destination_ip, destination_port):
     num_packets = 10
     total_bytes_sent = 0
     total_time_elapsed = 0
+    packets_received = 0
 
     for _ in range(num_packets):
         # 현재 시간 기록
@@ -21,6 +22,7 @@ def measure_network_performance(destination_ip, destination_port):
         # 수신 대기
         try:
             data, _ = sock.recvfrom(1024)
+            packets_received += 1
         except socket.timeout:
             print("Packet loss")
             continue
@@ -33,9 +35,13 @@ def measure_network_performance(destination_ip, destination_port):
         total_bytes_sent += len(data)
         total_time_elapsed += elapsed_time
 
+    if packets_received == 0:
+        print("No packets received.")
+        return
+
     # 네트워크 대역폭 및 평균 지연 시간 계산
     bandwidth = (total_bytes_sent * 8) / (total_time_elapsed * 1024 * 1024)
-    avg_latency = (total_time_elapsed / num_packets) * 1000
+    avg_latency = (total_time_elapsed / packets_received) * 1000
 
     print(f"Average Bandwidth: {bandwidth} Mbps")
     print(f"Average Latency: {avg_latency} ms")
@@ -44,9 +50,8 @@ def measure_network_performance(destination_ip, destination_port):
     sock.close()
 
 # 네트워크 성능 측정 대상 지정 (웹캠의 IP 주소와 포트 써줘)
-destination_ip = 
-destination_port = 
+destination_ip = "192.168.35.51"
+destination_port = 80
 
 # 네트워크 성능 평가 실행
 measure_network_performance(destination_ip, destination_port)
-
