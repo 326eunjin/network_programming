86b6c137e1e70287e9dc66b611ea960205ddcfce
wisenick1 <wisenick1@naver.com>
2023-05-24 08:00:29 +0900

    communication_flow.py 수정

--- communication_flow.py
+++ communication_flow.py
@@ -1,7 +1,31 @@
 from scapy.all import *
 
+import nmap
+
+# 네트워크 스캐너를 사용하여 iot 기기의 IP 주소를 스캔
+nm = nmap.PortScanner()
+nm.scan("192.168.35.0/24")  # 네트워크 주소 범위 지정
+
+# 스캔된 IP 주소 리스트 추출
+scanned_ips = []
+for host in nm.all_hosts():
+    # iot 기기의 특정 포트 번호가 열려있는지 확인
+    if nm[host].has_tcp(554) and nm[host]["tcp"][554]["state"] == "open":
+        scanned_ips.append(host)
+
 # 통신 흐름을 저장할 딕셔너리
 communication_flow = {}
+
+# 네트워크 스캐너를 사용하여 iot 기기의 IP 주소를 스캔
+nm = nmap.PortScanner()
+nm.scan("192.168.35.0/24")  # 네트워크 주소 범위 지정
+
+# 스캔된 IP 주소 리스트 추출
+scanned_ips = []
+for host in nm.all_hosts():
+        # iot 기기의 특정 포트 번호가 열려있는지 확인
+            if nm[host].has_tcp(554) and nm[host]["tcp"][554]["state"] == "open":
+                        scanned_ips.append(host)
 # protocol 횟수 카운트
 protocol_count = {}
 
@@ -34,8 +58,11 @@ def analyze_packet(packet):
         else:
             protocol_count[protocol] = 1
 
+#몇 초간 캡쳐할 건지(초 넣어줘)
+sniffingTime = input("Sniffing Time: ")
+
 # UDP 및 TCP 패킷 캡처
-sniff(filter="udp or tcp", prn=analyze_packet)
+sniff(filter="udp or tcp", prn=analyze_packet, timeout=int(sniffingTime))
 
 # 통신 흐름 출력
 for flow, count in communication_flow.items():
