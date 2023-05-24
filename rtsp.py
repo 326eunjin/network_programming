c6f466d7b472abd734ff9688629bc0e9cb3b03e4
326eunjin <326eunjin@naver.com>
2023-05-25 01:44:18 +0900

    add statistics

--- rtsp.py
+++ rtsp.py
@@ -1,12 +1,13 @@
 import sys
 import nmap
-from datetime import datetime
+from datetime import datetime, timedelta
 from scapy.all import *
 import matplotlib.pyplot as plt
 from collections import Counter
 
 packet_count = 0
 protocols = Counter()
+captured_packets = []
 
 nm = nmap.PortScanner()
 nm.scan("192.168.35.0/24")
@@ -19,12 +20,13 @@ for host in nm.all_hosts():
 
 if iot_ip is None:
     print("IoT 기기를 찾을 수 없습니다.")
-else:
-    print("IoT 기기의 IP 주소: ", iot_ip)
+    sys.exit()
+
+print("IoT 기기의 IP 주소:", iot_ip)
 
 
 def handle_tcp_packet(packet):
-    global packet_count, protocols
+    global packet_count, protocols, captured_packets
     if packet.haslayer(TCP):
         sport = packet[TCP].sport
         dport = packet[TCP].dport
@@ -38,10 +40,11 @@ def handle_tcp_packet(packet):
 
         packet_count += 1
         protocols["TCP"] += 1
+        captured_packets.append(packet)
 
 
 def handle_udp_packet(packet):
-    global packet_count, protocols
+    global packet_count, protocols, captured_packets
     if packet.haslayer(UDP):
         sport = packet[UDP].sport
         dport = packet[UDP].dport
@@ -55,10 +58,11 @@ def handle_udp_packet(packet):
 
         packet_count += 1
         protocols["UDP"] += 1
+        captured_packets.append(packet)
 
 
 def handle_rtp_packet(packet):
-    global packet_count, protocols
+    global packet_count, protocols, captured_packets
     if packet.haslayer(RTP):
         payload = packet[RTP].payload
         if payload:
@@ -89,6 +93,7 @@ def handle_rtp_packet(packet):
 
             packet_count += 1
             protocols["RTP"] += 1
+            captured_packets.append(packet)
 
 
 sniffingTime = input("Sniffing Time (in seconds): ")
@@ -120,14 +125,73 @@ if iot_ip:
         print("No Packet")
         sys.exit()
     else:
-        print("Total Packet: %s" % packet_count)
+        print("Total Packet:", packet_count)
 
         # Visualization - Protocol Distribution
         labels = protocols.keys()
         values = protocols.values()
 
+        plt.figure(figsize=(8, 6))
         plt.pie(values, labels=labels, autopct="%1.1f%%")
         plt.title("Protocol Distribution")
         plt.show()
+        print("Protocol Distribution: ")
+        for label, value in zip(labels, values):
+            print(f"{label}: {value} packets")
+
+        # Visualization - Packet Size Distribution
+        packet_sizes = [len(packet.payload) for packet in captured_packets]
+        plt.figure(figsize=(8, 6))
+        plt.hist(packet_sizes, bins=50)
+        plt.xlabel("Packet Size")
+        plt.ylabel("Frequency")
+        plt.title("Packet Size Distribution")
+        plt.show()
+        print("Packet Size Distribution")
+
+# ...
+
+        # Visualization - Traffic Analysis
+        intervals = []
+        packet_counts = []
+
+        current_time = start_time
+        while current_time <= end_time:
+            next_time = current_time + timedelta(seconds=1)
+            interval_packets = [
+                packet
+                for packet in captured_packets
+                if current_time <= datetime.fromtimestamp(packet.time) <= next_time
+            ]
+            intervals.append(current_time.strftime("%H:%M:%S"))
+            packet_counts.append(len(interval_packets))
+            current_time = next_time
+
+        # Determine the number of x-axis labels to display
+        max_labels = 10
+        num_labels = min(len(intervals), max_labels)
+
+        # Determine the step size for selecting the labels
+        step = max(len(intervals) // num_labels, 1)
+
+        # Select a subset of labels and counts for display
+        selected_intervals = intervals[::step]
+        selected_packet_counts = packet_counts[::step]
+
+        plt.figure(figsize=(12, 6))
+        plt.plot(intervals, packet_counts, marker="o")
+        plt.xlabel("Time")
+        plt.ylabel("Packet Count")
+        plt.title("Traffic Analysis")
+        plt.xticks(rotation=45)
+        plt.xticks(selected_intervals)  # Set the selected labels on the x-axis
+        plt.plot(selected_intervals, selected_packet_counts, marker="o")  # Plot the selected data points
+        plt.show()
+
+        print("Traffic Analysis:")
+        for time, count in zip(selected_intervals, selected_packet_counts):
+            print(f"{time}: {count} packets")
+
+
 else:
     print("프로그램을 실행할 수 없습니다.")
