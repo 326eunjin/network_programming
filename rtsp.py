612cbc25d87c0421f9b273b0adc7b612a8676214
wisenick1 <wisenick1@naver.com>
2023-05-25 15:47:03 +0900

    rtsp.py commnication flow.py 합치기

--- rtsp.py
+++ rtsp.py
@@ -25,6 +25,37 @@ if iot_ip is None:
 
 print("IoT 기기의 IP 주소:", iot_ip)
 
+# protocol 횟수 카운트
+protocol_count = {}
+
+def analyze_packet(packet):
+    if packet.haslayer(IP):
+        if packet.haslayer(UDP):
+            protocol = "UDP"
+            src = packet[IP].src
+            dst = packet[IP].dst
+            sport = packet[UDP].sport
+            dport = packet[UDP].dport
+        elif packet.haslayer(TCP):
+            protocol = "TCP"
+            src = packet[IP].src
+            dst = packet[IP].dst
+            sport = packet[TCP].sport
+            dport = packet[TCP].dport
+        else:
+            return
+
+        # 송신자와 수신자를 키로 사용하여 통신 흐름 저장
+        flow_key = (src, sport, dst, dport, protocol)
+        if flow_key in communication_flow:
+            communication_flow[flow_key] += 1
+        else:
+            communication_flow[flow_key] = 1
+
+        if protocol in protocol_count:
+            protocol_count[protocol] += 1
+        else:
+            protocol_count[protocol] = 1
 
 def handle_tcp_packet(packet):
     global packet_count, protocols, captured_packets, latency_values
@@ -103,6 +134,18 @@ def handle_rtp_packet(packet):
 sniffingTime = input("Sniffing Time (in seconds): ")
 start_time = datetime.now()
 end_time = start_time + timedelta(seconds=int(sniffingTime))
+
+# UDP 및 TCP 패킷 캡처
+sniff(filter="udp or tcp", prn=analyze_packet, timeout=int(sniffingTime))
+
+# 통신 흐름 출력
+for flow, count in communication_flow.items():
+    src, sport, dst, dport, protocol = flow
+    print(f"Flow: {protocol} {src}:{sport} -> {dst}:{dport}, Count: {count}")
+
+#프로토콜 횟수 분석 결과 출력
+for protocol, count in protocol_count.items():
+    print(f"Protocol: {protocol}, Count : {count}")
 
 if iot_ip:
     print("프로그램 시작")
