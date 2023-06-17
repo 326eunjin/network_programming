f9777e72529f16601574482d2afccde6c051277b
326eunjin <326eunjin@naver.com>
2023-06-17 21:15:38 +0900

    터질거같아

--- work.c
+++ work.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -8,7 +9,16 @@
 #include <openssl/sha.h>
 
 #define BUFSIZE 256
-#define MAX_PROCESSES 4
+#define MAX_PROCESSES 10
+
+void errProc(const char *str) {
+    fprintf(stderr, "%s: %s \n", str, strerror(errno));
+    exit(1);
+}
+
+void errPrint(const char *str) {
+    fprintf(stderr, "%s: %s \n", str, strerror(errno));
+}
 
 void calculate_hash(const char *challenge, unsigned int nonce, unsigned char *hash)
 {
@@ -52,6 +62,7 @@ void find_nonce(const int write_fd, const char *challenge, const int difficulty,
 
         if (check_difficulty(hash, difficulty))
         {
+            printf("찾은 nonce 값: %u\n", nonce); // nonce 값을 출력
             write(write_fd, &nonce, sizeof(unsigned int)); // Write the nonce to the pipe
             close(write_fd);
             return;
@@ -65,120 +76,98 @@ void find_nonce(const int write_fd, const char *challenge, const int difficulty,
 
 int main(int argc, char **argv)
 {
-
-    unsigned int nonce;
     int clntSd;
-    int count = 0;
-    struct sockaddr_in clnt_addr;
-    char challenge[BUFSIZE]; // 챌린지값 저장
+    struct sockaddr_in srvAddr;
+    char rBuff[BUFSIZ];
     int difficulty;
+    char challenge[BUFSIZ];
+    unsigned int nonce;
 
-    unsigned int num_nonce_per_process = (UINT32_MAX + 1) / 8;
-    pid_t pids[8];
-    int pipes[8][2];
+    unsigned int num_nonce_per_process = (UINT32_MAX + 1) / MAX_PROCESSES;
+    pid_t pids[MAX_PROCESSES];
+    int pipes[MAX_PROCESSES][2];
 
-    clntSd = socket(AF_INET, SOCK_STREAM, 0); // 클라이언트 소켓
+    if (argc != 3)
+    {
+        printf("사용법: %s [서버IP] [포트번호] \n", argv[0]);
+        exit(1);
+    }
+
+    clntSd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (clntSd == -1)
+        errProc("socket");
+
+    memset(&srvAddr, 0, sizeof(srvAddr));
+    srvAddr.sin_family = AF_INET;
+    srvAddr.sin_addr.s_addr = inet_addr(argv[1]);
+    srvAddr.sin_port = htons(atoi(argv[2]));
+
+    if (connect(clntSd, (struct sockaddr *)&srvAddr, sizeof(srvAddr)) == -1)
+        errProc("connect");
+
+    // 난이도 값을 서버로부터 받음
+    read(clntSd, &difficulty, sizeof(difficulty));
+    printf("서버로부터 난이도 값(%d)을 받았습니다.\n", difficulty);
+
+    // 도전 값을 서버로부터 받음
+    read(clntSd, challenge, BUFSIZ);
+    printf("서버로부터 도전 값(%s)을 받았습니다.\n", challenge);
+    
+    for (unsigned int i = 0; i < MAX_PROCESSES; i++)
     {
-        perror("socket");
+        if (pipe(pipes[i]) == -1)
+        {
+            fprintf(stderr, "Failed to create pipe.\n");
+            return 1;
+        }
+
+        unsigned int start_nonce = i * num_nonce_per_process;
+        unsigned int end_nonce = (i + 1) * num_nonce_per_process - 1;
+
+        if (i == MAX_PROCESSES - 1)
+        {
+            end_nonce = UINT32_MAX;
+        }
+
+        pid_t pid = fork();
+
+        if (pid < 0)
+        {
+            fprintf(stderr, "Fork failed.\n");
+            return 1;
+        }
+        else if (pid == 0)
+        {
+            close(pipes[i][0]); // Close the read end of the pipe in the child process
+            find_nonce(pipes[i][1], challenge, difficulty, start_nonce, end_nonce);
+            exit(0);
+        }
+        else
+        {
+            close(pipes[i][1]); // Close the write end of the pipe in the parent process
+            pids[i] = pid;
+        }
+    }
+
+    for (unsigned int i = 0; i < MAX_PROCESSES; i++)
+    {
+        if (read(pipes[i][0], &nonce, sizeof(unsigned int)) != -1)
+        {
+            break;
+        }
+    }
+    for (unsigned int i = 0; i < MAX_PROCESSES; i++)
+    {
+        close(pipes[i][0]); // Close the read end of the pipe in the parent process
+    }
+
+    // 서버로 nonce 값 전송
+    if (write(clntSd, &nonce, sizeof(nonce)) == -1)
+    {
+        perror("write");
         exit(1);
     }
 
-    // 서버와 tcp connection
-    memset(&clnt_addr, 0, sizeof(clnt_addr));
-    clnt_addr.sin_family = AF_INET;
-    clnt_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    clnt_addr.sin_port = htons(atoi(argv[2]));
-
-    if (connect(clntSd, (struct sockaddr *)&clnt_addr, sizeof(clnt_addr)) == -1)
-    {
-        perror("connect");
-        close(clntSd);
-        exit(1);
-    }
-    printf("통신완료\n");
-    // 서버에서 전송한 challenge와 난이도 받기
-    while (1)
-    {
-        if (count == 0)
-        {
-            // Challenge and difficulty reading
-            memset(challenge, 0, sizeof(challenge));
-            if (read(clntSd, challenge, sizeof(challenge) - 1) == -1)
-            {
-                perror("read");
-                exit(1);
-            }
-            count++;
-            printf("challenge: %s\n", challenge);
-        }
-
-        if(count == 1)
-        {
-            if (read(clntSd, &difficulty, sizeof(int)) == -1)
-            {
-                perror("read");
-                exit(1);
-            }
-            printf("difficulty: %d\n", difficulty);
-        }
-        // nonce 값 계산
-        for (unsigned int i = 0; i < 8; i++)
-        {
-            if (pipe(pipes[i]) == -1)
-            {
-                fprintf(stderr, "Failed to create pipe.\n");
-                return 1;
-            }
-
-            unsigned int start_nonce = i * num_nonce_per_process;
-            unsigned int end_nonce = (i + 1) * num_nonce_per_process - 1;
-
-            if (i == 8 - 1)
-            {
-                end_nonce = UINT32_MAX;
-            }
-
-            pid_t pid = fork();
-
-            if (pid < 0)
-            {
-                fprintf(stderr, "Fork failed.\n");
-                return 1;
-            }
-            else if (pid == 0)
-            {
-                close(pipes[i][0]); // Close the read end of the pipe in the child process
-                find_nonce(pipes[i][1], challenge, difficulty, start_nonce, end_nonce);
-                exit(0);
-            }
-            else
-            {
-                close(pipes[i][1]); // Close the write end of the pipe in the parent process
-                pids[i] = pid;
-            }
-        }
-
-        for (unsigned int i = 0; i < 8; i++)
-        {
-            if (read(pipes[i][0], &nonce, sizeof(unsigned int)) != -1)
-            {
-                break;
-            }
-        }
-
-        for (unsigned int i = 0; i < 8; i++)
-        {
-            close(pipes[i][0]); // Close the read end of the pipe in the parent process
-        }
-
-        // 서버로 nonce 값 전송
-        if (write(clntSd, nonce, sizeof(nonce)) == -1)
-        {
-            perror("write");
-            exit(1);
-        }
-    }
     close(clntSd);
 
     return 0;
