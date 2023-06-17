4399e161c833d3d9cbf1f6d9f670f1cd93094f72
326eunjin <326eunjin@naver.com>
2023-06-18 05:29:11 +0900

    client.c 기본틀 nonce 계싼 안함

--- client.c
+++ client.c
@@ -1,6 +1,9 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <netinet/in.h>
+#include <openssl/sha.h>
+#include <limits.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,46 +11,31 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <time.h>
 
 void errProc(const char *str);
 void errPrint(const char *str);
-
-unsigned int calculate_nonce(int start, int end, int difficulty, const char *challenge) {
-    for (unsigned int nonce = start; nonce <= end; nonce++) {
-        // nonce를 문자열로 변환합니다.
-        char nonceStr[20];
-        snprintf(nonceStr, sizeof(nonceStr), "%u", nonce);
-
-        // challenge와 nonce를 합친 문자열을 생성합니다.
-        char combinedStr[BUFSIZ];
-        snprintf(combinedStr, sizeof(combinedStr), "%s%s", challenge, nonceStr);
-
-        // 해시를 계산합니다. (여기에서는 단순히 문자열의 길이를 사용하였습니다.)
-        unsigned int hash = strlen(combinedStr);
-
-        // 해시 접두사(prefix)와 일치하는지 확인합니다.
-        if (hash <= difficulty) {
-            return nonce;
-        }
-    }
-
-    return -1;  // nonce를 찾지 못한 경우 -1을 반환합니다.
-}
+unsigned int calculateNonce(unsigned int start, unsigned int end, int difficulty, const char *challenge);
 
 int main(int argc, char **argv) {
-    int clntSd;
+    int srvSd;
     struct sockaddr_in srvAddr;
+    int strLen;
+    char rBuff[BUFSIZ];
     int difficulty;
     char challenge[BUFSIZ];
-    unsigned int nonce;
+    unsigned int nonce; // Changed nonce type to unsigned int
+    int numProcesses = 4; // Number of child processes to create
 
     if (argc != 3) {
-        printf("사용법: %s [서버IP] [포트번호] \n", argv[0]);
+        printf("사용법: %s [서버IP] [포트번호]\n", argv[0]);
         exit(1);
     }
 
-    clntSd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (clntSd == -1)
+    printf("클라이언트 시작...\n");
+
+    srvSd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (srvSd == -1)
         errProc("socket");
 
     memset(&srvAddr, 0, sizeof(srvAddr));
@@ -55,57 +43,98 @@ int main(int argc, char **argv) {
     srvAddr.sin_addr.s_addr = inet_addr(argv[1]);
     srvAddr.sin_port = htons(atoi(argv[2]));
 
-    if (connect(clntSd, (struct sockaddr *)&srvAddr, sizeof(srvAddr)) == -1)
+    if (connect(srvSd, (struct sockaddr *)&srvAddr, sizeof(srvAddr)) == -1)
         errProc("connect");
 
-    // 난이도 값을 서버로부터 받음
-    read(clntSd, &difficulty, sizeof(difficulty));
-    printf("서버로부터 난이도 값(%d)을 받았습니다.\n", difficulty);
+    // 난이도 값 수신
+    read(srvSd, &difficulty, sizeof(difficulty));
+    printf("난이도: %d\n", difficulty);
 
-    // 도전 값을 서버로부터 받음
-    read(clntSd, challenge, BUFSIZ);
-    printf("서버로부터 도전 값(%s)을 받았습니다.\n", challenge);
+    // 도전 값 수신
+    read(srvSd, challenge, sizeof(challenge));
+    printf("도전 값: %s\n", challenge);
 
-    int num_processes = 4; // 프로세스 수를 설정하세요
-
-    int range = 100000000 / num_processes;  // 범위를 적절히 나누어 줍니다.
-
-    for (int i = 0; i < num_processes; i++) {
+    // Fork child processes
+    for (int i = 0; i < numProcesses; i++) {
         pid_t pid = fork();
-        if (pid == -1) {
-            errProc("fork");
-        } else if (pid == 0) { /* 자식 프로세스 */
-            int start = i * range;
-            int end = (i + 1) * range - 1;
-            nonce = calculate_nonce(start, end, difficulty, challenge);
 
-            printf("프로세스 %d: 범위(%d ~ %d), 계산된 nonce 값(%u)\n", getpid(), start, end, nonce);
+        if (pid == 0) {
+            // Child process
+            unsigned int start = UINT_MAX / numProcesses * i;
+            unsigned int end = UINT_MAX / numProcesses * (i + 1) - 1;
 
-            if (write(clntSd, &nonce, sizeof(nonce)) == -1) {
-                errPrint("write");
-                exit(1);
-            }
+            nonce = calculateNonce(start, end, difficulty, challenge);
 
-            close(clntSd);
+            // nonce 값 전송
+            write(srvSd, &nonce, sizeof(nonce));
+            printf("Child process %d: Nonce %u 값을 서버에 전송하였습니다.\n", getpid(), nonce);
+
+            close(srvSd);
             exit(0);
+        } else if (pid < 0) {
+            // Error occurred
+            errProc("fork");
         }
     }
 
-    // 부모 프로세스는 자식 프로세스의 실행을 기다림
-    for (int i = 0; i < num_processes; i++) {
+    // Parent process
+    for (int i = 0; i < numProcesses; i++) {
         wait(NULL);
     }
 
-    close(clntSd);
+    printf("모든 child process가 완료되었습니다.\n");
+    close(srvSd);
 
     return 0;
 }
 
 void errProc(const char *str) {
-    fprintf(stderr, "%s: %s \n", str, strerror(errno));
+    fprintf(stderr, "%s: %s\n", str, strerror(errno));
     exit(1);
 }
 
 void errPrint(const char *str) {
-    fprintf(stderr, "%s: %s \n", str, strerror(errno));
+    fprintf(stderr, "%s: %s\n", str, strerror(errno));
+}
+
+unsigned int calculateNonce(unsigned int start, unsigned int end, int difficulty, const char *challenge) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    char hexString[SHA256_DIGEST_LENGTH * 2 + 1];
+    unsigned int nonce;
+
+    for (nonce = start; nonce <= end; nonce++) {
+        // Convert nonce to string representation
+        char nonceString[sizeof(nonce) * 2 + 1];
+        sprintf(nonceString, "%08x", nonce);
+
+        // Concatenate the challenge and nonce
+        char data[BUFSIZ];
+        snprintf(data, BUFSIZ, "%s%s", challenge, nonceString);
+
+        // Calculate the SHA256 hash
+        SHA256((unsigned char *)data, strlen(data), hash);
+
+        // Convert the hash to a hex string
+        for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+            sprintf(&hexString[i * 2], "%02x", hash[i]);
+        }
+        hexString[SHA256_DIGEST_LENGTH * 2] = '\0';
+
+        // Check if the hash meets the difficulty requirement
+        int match = 1;
+        for (int i = 0; i < difficulty; i++) {
+            if (hexString[i] != '0') {
+                match = 0;
+                break;
+            }
+        }
+
+        if (match) {
+            printf("Child process %d: Nonce found: %u\n", getpid(), nonce);
+            return nonce;
+        }
+    }
+
+    printf("Child process %d: Nonce not found within the specified range.\n", getpid());
+    return 0;
 }
